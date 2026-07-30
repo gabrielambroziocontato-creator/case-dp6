5dcb496fe2c0a357d5d7c4e7d58d68a8e88be6d9
gabrielambroziocontato-creator <gabrielambroziocontato@gmail.com>
2026-07-30 01:21:46 -0300

    Form start better precision

--- js/main.js
+++ js/main.js
@@ -14,11 +14,14 @@
   jQuery('.cards-montadoras').on('click tap', '.card-montadoras', function () {
     var data = jQuery(this).data();
     var content = jQuery('.info-montadora[data-id="' + data.id + '"]').html();
+
     lb.find('.lightbox-icon')
       .attr('src', 'img/montadoras/' + data.id + '.png')
       .attr('alt', data.name);
+
     lb.find('.lightbox-title').text(data.name);
     lbContent.html(content).scrollTop(0);
+
     setTimeout(function () {
       body.addClass('lightbox-open');
     }, 200);
@@ -39,6 +42,7 @@
   jQuery('.menu')
     .on('click tap', '.menu-lista-sublista > a', function (e) {
       e.preventDefault();
+
       jQuery(this)
         .parent()
         .toggleClass('fechado')
@@ -48,6 +52,7 @@
     .on('click tap', '.menu-sublista-link', function (e) {
       if (this.pathname === location.pathname) {
         body.removeClass('menu-open');
+
         if (!this.hash) {
           e.preventDefault();
           main.scrollTop(0);
@@ -56,22 +61,33 @@
     });
 
   var submit = jQuery('.contato button[type="submit"]');
+
   jQuery('.contato').on('submit', function (e) {
+    var form = this;
+
     submit.attr('disabled', 'disabled');
     e.preventDefault();
+
     setTimeout(function () {
       console.log('enviado');
+
       lb.find('.lightbox-icon')
         .attr('src', 'img/logo-dp6-square.png')
         .attr('alt', 'Logo DP6');
+
       lb.find('.lightbox-title').text('Contato enviado');
       lbContent.html('Obrigado pelo seu contato!').scrollTop(0);
+
       setTimeout(function () {
         body.addClass('lightbox-open');
+
         window.dataLayer = window.dataLayer || [];
         window.dataLayer.push({
-        event: 'view_form_success'
-  });
+          event: 'view_form_success',
+          form_id: form.id || '',
+          form_name: form.name || ''
+        });
+
         submit.removeAttr('disabled');
       }, 200);
     }, Math.random() * 2000);
@@ -86,30 +102,24 @@
 })(jQuery);
 
 (function () {
-  var form = document.querySelector('#form_contato');
-
-  if (!form) return;
-
-  var started = false;
-
   function trackFormStart(event) {
-    if (started) return;
+    var form = event.target.form;
 
-    if (!event.target.matches('input, textarea, select')) {
+    if (!form || form.dataset.formStarted) {
       return;
     }
 
-    started = true;
+    form.dataset.formStarted = 'true';
 
     window.dataLayer = window.dataLayer || [];
     window.dataLayer.push({
-      event: 'form_start'
+      event: 'form_start',
+      form_id: form.id || '',
+      form_name: form.name || '',
+      form_destination: (form.action || window.location.href).split('#')[0]
     });
-
-    form.removeEventListener('input', trackFormStart);
-    form.removeEventListener('change', trackFormStart);
   }
 
-  form.addEventListener('input', trackFormStart);
-  form.addEventListener('change', trackFormStart);
+  document.addEventListener('input', trackFormStart, true);
+  document.addEventListener('change', trackFormStart, true);
 })();
